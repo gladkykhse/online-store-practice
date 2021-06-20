fb2594c65f3d75ea643918841dd41bd1ba53bfc7
Gladkykh Sviatoslav <gladkykhse@gmail.com>
2021-06-21 00:36:36 +0300

    added some packages, created basic ejs files

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from "express"
+import path from "path"
+
 
 const app = express()
 const PORT = process.env.PORT ?? 3000
+const __dirname = path.resolve()
+
+
+app.use(express.static(path.resolve(__dirname,"static")))
+
+
+app.set("view engine","ejs")
+
+
+app.get("/", (req, res) => {
+    res.render("index",{title:"Main page"})
+})
 
 app.listen(PORT, () => {
     console.log(`Server has been started on port ${PORT}...`)
