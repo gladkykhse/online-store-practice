a20245915df3430047596ca6b367692f78c729a0
Gladkykh Sviatoslav <gladkykhse@gmail.com>
2021-07-25 20:25:34 +0300

    product blocks on page

--- static/main.js
+++ static/main.js
@@ -5,5 +5,30 @@ const getResponse = async (url) => {
 }
 
 getResponse("http://localhost:3000/data").then((data) => {
-    console.log(data)
+    for (let i = 0; i < data["products"].length; i++)
+    {
+        let smallName = ""
+        let smallDesc = ""
+
+        if (data["products"][i]["name"].length > 20) {
+            smallName = data["products"][i]["name"].slice(0,20) + "..."
+        } else {
+            smallName = data["products"][i]["name"]
+        }
+
+        if (data["products"][i]["description"].length > 105) {
+            smallDesc = data["products"][i]["description"].slice(0,105) + "..."
+        } else {
+            smallDesc = data["products"][i]["description"]
+        }
+
+
+
+        document.getElementById("allProds").innerHTML += `<div id="prod"><div id="prodName">${smallName}</div><br><div id="prodDesc">${smallDesc}</div></div>`
+    }
 })
+
+
+
+
+
